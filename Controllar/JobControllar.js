cfcbb7f5def822618279d07ce13546c6c8859013
mannumishra <mannumishra2207@outlook.com>
2024-05-27 14:51:33 +0530

    add chnages

--- Controllar/JobControllar.js
+++ Controllar/JobControllar.js
@@ -19,7 +19,11 @@ const createJob = async (req, res) => {
             data: data
         })
     } catch (error) {
-        console.log(error)
+        res.status(500).json({
+            success: false,
+            mess: "Internal Server Error",
+            data: data
+        })
     }
 }
 
@@ -32,11 +36,82 @@ const getRecord = async (req, res) => {
             data: data
         })
     } catch (error) {
-        console.log(error)
+        res.status(500).json({
+            success: false,
+            mess: "Internal Server Error",
+            data: data
+        })
+    }
+}
+const getSingleRecord = async (req, res) => {
+    try {
+        let data = await jobdetails.findOne({ _id: req.params._id })
+        res.status(200).json({
+            success: true,
+            mess: "Record Found",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            mess: "Internal Server Error",
+            data: data
+        })
+    }
+}
+const updateJob = async (req, res) => {
+    try {
+        let data = await jobdetails.findOne({ _id: req.params._id })
+        if (data) {
+            data.jobpost = req.body.jobpost ?? data.jobpost
+            data.experience = req.body.experience ?? data.experience
+            data.qualification = req.body.qualification ?? data.qualification
+            data.packageanual = req.body.packageanual ?? data.packageanual
+            data.organisationname = req.body.organisationname ?? data.organisationname
+            data.address = req.body.address ?? data.address
+            data.state = req.body.state ?? data.state
+            data.pincode = req.body.pincode ?? data.pincode
+            data.contact = req.body.contact ?? data.contact
+            data.mobile = req.body.mobile ?? data.mobile
+            data.email = req.body.email ?? data.email
+            await data.save()
+            res.status(200).json({
+                success: true,
+                mess: "Job Post updated successfully",
+                data: data
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            mess: "Internal Server Error",
+            data: data
+        })
+    }
+}
+
+const deleteJob = async (req, res) => {
+    try {
+        let data = await jobdetails.findOne({ _id: req.params._id })
+        await data.deleteOne()
+        res.status(200).json({
+            success: true,
+            mess: "Job Deleted Successfully",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            mess: "Internal Server Error",
+            data: data
+        })
     }
 }
 
 module.exports = {
     createJob: createJob,
-    getRecord:getRecord
+    getRecord: getRecord,
+    updateJob: updateJob,
+    getSingleRecord: getSingleRecord,
+    deleteJob:deleteJob
 }
--- Routes/JobRouter.js
+++ Routes/JobRouter.js
@@ -1,8 +1,11 @@
-const { createJob, getRecord } = require("../Controllar/JobControllar")
+const { createJob, getRecord, updateJob, getSingleRecord, deleteJob } = require("../Controllar/JobControllar")
 
 const jobrouter = require("express").Router()
 
-jobrouter.post("/job" , createJob)
-jobrouter.get("/job" , getRecord)
+jobrouter.post("/job", createJob)
+jobrouter.get("/job", getRecord)
+jobrouter.get("/job/:_id", getSingleRecord)
+jobrouter.put("/job", updateJob)
+jobrouter.delete("/job/:_id", deleteJob)
 
 module.exports = jobrouter
