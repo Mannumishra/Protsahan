09a0a272aaa8cf0e3bbb44b6b8fa459a067bf25d
mannumishra <mannumishra2207@outlook.com>
2024-05-27 17:35:38 +0530

    add chnage

--- Controllar/ContactControlar.js
+++ Controllar/ContactControlar.js
@@ -3,7 +3,6 @@ const nodemailer = require("nodemailer")
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,
     auth: {
         user: process.env.MAIL_SENDER,
         pass: process.env.MAIL_PASS
