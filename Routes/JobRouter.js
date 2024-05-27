95964dac53b2acd5c500a331af0268f5d7ae1410
mannumishra <mannumishra2207@outlook.com>
2024-05-27 15:06:10 +0530

    add changes

--- Routes/JobRouter.js
+++ Routes/JobRouter.js
@@ -5,7 +5,7 @@ const jobrouter = require("express").Router()
 jobrouter.post("/job", createJob)
 jobrouter.get("/job", getRecord)
 jobrouter.get("/job/:_id", getSingleRecord)
-jobrouter.put("/job", updateJob)
+jobrouter.put("/job/:_id", updateJob)
 jobrouter.delete("/job/:_id", deleteJob)
 
 module.exports = jobrouter
