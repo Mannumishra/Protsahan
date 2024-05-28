0d7b671514af6692ea07d3e7d7a602ed7731b818
mannumishra <mannumishra2207@outlook.com>
2024-05-28 17:25:01 +0530

    add chnages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const jobrouter = require("./Routes/JobRouter")
 const cors = require("cors")
 const contactrouter = require("./Routes/ContactRouter")
+const joinrouter = require("./Routes/JoinRouter")
+const volunterRouter = require("./Routes/Volunteer")
 require("dotenv").config()
 require("./Db/ConnectDb")
 
@@ -11,6 +13,8 @@ app.use(express.json())
 app.use(cors())
 app.use("/api", jobrouter)
 app.use("/api", contactrouter)
+app.use("/api", joinrouter)
+app.use("/api", volunterRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running at ${process.env.PORT}.`)
