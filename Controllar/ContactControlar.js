34b9781c1b3f5934d617e10b7a0c37622c429dee
mannumishra <mannumishra2207@outlook.com>
2024-05-29 16:05:38 +0530

    add chnages

--- Controllar/ContactControlar.js
+++ Controllar/ContactControlar.js
@@ -4,55 +4,57 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     auth: {
-        user: process.env.MAIL_SENDER,
-        pass: process.env.MAIL_PASS
+        user: "mannu22072000@gmail.com",
+        pass: "vcen opfp tnuj kszu"
     }
 })
 
 const createContact = async (req, res) => {
     try {
-        let { name, email, subject, message, city, country, number, address } = req.body
+        let { name, email, subject, message, city, country, number, address } = req.body;
         if (!name || !email || !subject || !message || !city || !country || !number || !address) {
             return res.status(403).json({
                 success: false,
-                mess: "Fill All Required Filelds"
-            })
+                mess: "Fill All Required Fields"
+            });
         }
-        const data = new contact({ name, email, subject, message, city, country, number, address })
-        console.log(email)
-        await data.save()
-        console.log("dataemails" ,data.email)
-        mailOptions = {
+
+        const data = new contact({ name, email, subject, message, city, country, number, address });
+        await data.save();
+        const mailOptions = {
             from: data.email,
             to: process.env.MAIL_SENDER,
-            subject: "A New Contact Recieve ",
+            subject: "A New Contact Received",
             text: `
-            Name ${data.name}
-            subject ${data.subject}
-            message ${data.message}
-            city ${data.city}
-            country ${data.country}
-            number ${data.number}
-            address ${data.address}
-                  `
-        }
-        transporter.sendMail(mailOptions, ((error) => {
+            Name: ${name}
+            Subject: ${subject}
+            Message: ${message}
+            City: ${city}
+            Country: ${country}
+            Number: ${number}
+            Address: ${address}
+            `
+        };
+        console.log(mailOptions)
+        transporter.sendMail(mailOptions, (error) => {
             if (error) {
-                console.log(error)
+                console.error("Error sending email:", error);
             }
-        }))
+        });
+
         res.status(200).json({
             success: true,
             mess: "Contact Created Successfully",
             data: data
-        })
+        });
     } catch (error) {
+        console.error("Error creating contact:", error);
         res.status(500).json({
             success: false,
             mess: "Internal Server Error"
-        })
+        });
     }
-}
+};
 
 const getRecord = async (req, res) => {
     try {
