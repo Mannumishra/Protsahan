c5cf15f982ef10416e63497fd0ffec376f03396c
mannumishra <mannumishra2207@outlook.com>
2024-06-07 18:43:50 +0530

    add chnages

--- Controllar/VolunteerControllar.js
+++ Controllar/VolunteerControllar.js
@@ -14,14 +14,14 @@ const transporter = nodemailer.createTransport({
 const createRecord = async (req, res) => {
     try {
         // console.log(req.body)
-        let { donation, sirName, firstName, lastName, dob, panNo, email, mobile, address, country, state, city, pinCode, citizenship, helpMessage, money } = req.body;
-        if (!donation || !sirName || !firstName || !lastName || !dob || !panNo || !email || !mobile || !address || !country || !state || !city || !pinCode || !citizenship || !helpMessage || !money) {
+        let { donation, sirName, firstName, lastName, dob, panNo, email, mobile, address, country, state, city, pinCode, citizenship, helpMessage, money ,intersted } = req.body;
+        if (!donation || !sirName || !firstName || !lastName || !dob || !panNo || !email || !mobile || !address || !country || !state || !city || !pinCode || !citizenship || !helpMessage || !money || !intersted) {
             return res.status(403).json({
                 success: false,
                 mess: "Fill all required fields"
             });
         } else {
-            const data = new volunteer({ donation, sirName, firstName, lastName, dob, panNo, email, mobile, address, country, state, city, pinCode, citizenship, helpMessage, money });
+            const data = new volunteer({ donation, sirName, firstName, lastName, dob, panNo, email, mobile, address, country, state, city, pinCode, citizenship, helpMessage, money ,intersted });
             await data.save();
             const mailOptionsApplicant = {
                 from: "info@prothsahanteam.org",
